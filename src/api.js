6368e4012544318a459ab5fca8b9b325c1a1b134
Вікторія Юрченко <viktoriau26@gmail.com>
2021-07-22 15:11:04 +0300

    +

--- src/api.js
+++ src/api.js
@@ -1,23 +1,30 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 export default class Cards {
   constructor() {
     this.page = 1;
     this.searchQuery = '';
   }
-  fetchAPI(q) {
+  fetchCards = async (q) => {
     const options = {
       key: '22593683-900dbddd4b86d221bedd65f3e',
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true'
     }
-    return fetch(`${BASE_URL}?
-    key=${options.key}&
-    q=${q}&
-    image_type=${options.image_type}&
-    orientation=${options.orientation}&
-    safesearch=${options.safesearch}`)
-    .then
-}
+    const response = await axios.get(`${BASE_URL}?key=${options.key}&q=${q}&image_type=${options.image_type}&orientation=${options.orientation}&safesearch=${options.safesearch}&per_page=40&page=${this.page}`);
+    return response.data;
+  }
+  nextPage() {
+    this.page += 1;
+    console.log(this.page);
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  clearSearchForm() {
+    this.searchQuery = '';
+  }
 }
   
--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
 import './sass/main.scss';
 import './api.js'
-import axios from 'axios';
+import Cards from './api.js'
+import SimpleLightbox from "simplelightbox";
+import Notiflix from "notiflix";
+import Template from './partials/templates/cardMarkup.hbs'
   
 const cardsContainer = document.querySelector('.gallery');
 const searchForm = document.getElementById('search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+loadMoreBtn.style.display = 'none';
+searchForm.addEventListener('submit', onSubmit);
 
-// console.log(searchForm);
+const newCard = new Cards();
+async function onSubmit(e) {
+  e.preventDefault();
+  loadMoreBtn.style.display = 'none';
+  cardsContainer.innerHTML = "";
+  const inputValue = e.currentTarget.elements.searchQuery.value;
+  localStorage.setItem("inputValue", JSON.stringify(inputValue));
+  newCard.resetPage();
+  renderMarkup();
+}
+
+function renderMarkup() {
+  const inputValue = localStorage.getItem("inputValue");
+  newCard.fetchCards(inputValue)
+    .then((response) => {
+      if (response.total > 0) {
+        onSuccess(response);
+      } else if (response.total === 0) {
+        onFailure();
+      }
+    })
+    // .catch((error) => console.error(error));
+}
+
+function onImageClick(e) {
+  e.preventDefault();
+  openLightBox();
+};
+
+function onLoadMore() {
+  openLightBox();
+  gallery.refresh();
+  newCard.nextPage();
+  renderMarkup();
+};
+
+function onSuccess(response) {
+  const cardMarkup = Template(response);
+  cardsContainer.insertAdjacentHTML("beforeend", cardMarkup);
+  Notiflix.Notify.success(
+    `Hooray! We found ${response.totalHits} images.`,
+    {
+      timeout: 3000,
+    },
+  );
+  document.querySelector('.photo-card').addEventListener('click', onImageClick);
+  loadMoreBtn.style.display = 'block';
+  loadMoreBtn.addEventListener('click', onLoadMore);
+}
+
+function onFailure() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.',
+    {
+      timeout: 3000,
+    },
+  );
+};
+
+function openLightBox() {
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function () {
+	  // do something…
+  });
+}
